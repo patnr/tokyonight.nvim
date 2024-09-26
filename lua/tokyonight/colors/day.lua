1103369d90bdc489ad1f72e61c0afc5667d07583
patnr <patrick.n.raanes@gmail.com>
2024-09-26 12:34:37 +0200

    Revert "Clearer day (ipython term) colors"
    This reverts commit 4620fa2654034a73ed346c2986f07c9f2ae11997.

--- lua/tokyonight/colors/day.lua
+++ lua/tokyonight/colors/day.lua
@@ -12,12 +12,5 @@ return function(opts)
 
   Util.invert(colors)
   colors.bg_dark = Util.blend(colors.bg, 0.9, colors.fg)
-  -- Make bg fully white
-  colors.bg = "#ffffff"
-  colors.fg = "#1a1a33"
-  -- colors.green = "#b31a1a"
-  -- There is no point setting vim.g.terminal_color_N (via `theme.terminal()`)
-  -- because they need to be set before the terminal is opened,
-  -- while Styler is called after terminal is opened.
   return colors
 end
